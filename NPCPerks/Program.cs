e4c1fca9c5ca1fe1f382de2720cedb2d3d2de525
Inês Barradas <inesmartinsbarradas1996@gmail.com>
2020-03-26 11:51:14 +0000

    exercices

---
+++ NPCPerks/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace NPCPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variáveis
+            Perks[] perks;
+            string aux;
+            int numNPCs;
+            
+            //Pedir ao user o nº de NPCs 
+            Console.WriteLine("Insira nº de NPC: ");
+            aux = Console.ReadLine();
+            numNPCs = Convert.ToInt32(aux);
+            perks = new Perks[numNPCs];
+
+            //Pedir cada NPC, pedir caracteristicas
+            for (int i = 0; i < numNPCs; i++)
+            {
+                Console.WriteLine($"NPC {i} tem Stealth (s/n)? ");
+                aux = Console.ReadLine();
+                if (aux.ToLower() == "s")
+                {
+                    perks[i] |= Perks.Stealth;
+                }
+
+                Console.WriteLine($"NPC {i} tem Combat (s/n)? ");
+                aux = Console.ReadLine();
+                if (aux.ToLower() == "s")
+                {
+                    perks[i] |= Perks.Combat;
+                }
+
+                Console.WriteLine($"NPC {i} tem Lockpick (s/n)? ");
+                aux = Console.ReadLine();
+                if (aux.ToLower() == "s")
+                {
+                    perks[i] |= Perks.Lockpick;
+                }
+
+                Console.WriteLine($"NPC {i} tem Luck (s/n)? ");
+                aux = Console.ReadLine();
+                if (aux.ToLower() == "s")
+                {
+                    perks[i] |= Perks.Luck;
+                }
+
+            }
+
+            //Mostrar no ecr as caracteristicas
+            Console.WriteLine();
+
+            for (int i = 0; i < numNPCs; i++)
+            {
+                Perks p = Perks.Combat | Perks.Luck;
+                Console.WriteLine($"Perks do NPC {i}: {perks[i]}");
+
+                if ((perks[i] & p) == p)
+                {
+                    Console.WriteLine("\tYou shall win all figths");
+                }
+            }
+
+        }
+    }
+}
---
+++ PercorreString/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variáveis
+            string str;
+            int linhas, colunas;
+            float[,] matriz;
+            float somaDasMedias _= 0;
+
+            //Pedir ao user nº Linhas de matriz
+            Console.Write("Insira nº de Linhas: ");
+            str = Console.ReadLine();
+            Linhas = int.Parse(str);
+
+            //Pedir o nº  de colunas da matiz
+            Console.Write("Insira nº de Colunas: ");
+            str = Console.ReadLine();
+            Linhas = int.Parse(str);
+
+            //inicializar a matriz
+            matriz = new float[linhas, colunas];
+
+            //pedir ao user os valores float para colocar na matriz
+            for (int i=0; i < matriz.GetLength(1); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Insira o valor em ({i},{j}): ");
+                    str = Console.WriteLine();
+                    maatriz[i, j] = Convert.ToSingle(str);
+                }
+
+            //mostrar
+            for (int i=0; i < matriz.GetLength(1); j++)
+            {
+                float soma = 0, media;
+                Console.Write("| ");
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    soma += matriz[i, j];
+                    Console.WriteLine($"{matriz[i, j], 10:f2} ");
+                }
+                media =soma /matriz.GetLength(1);
+                Console.WriteLine($"| -> {media,10:f3}");
+                somaDasMedias += media;
+            }
+
+            //Mostrar soma das médias
+            Console.WriteLine("Soma das medias: {0,10:f3}", somaDasMedias);
+
+
+        }
+    }
+}
